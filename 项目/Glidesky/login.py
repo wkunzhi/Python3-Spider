c1d939954a7324acd816c0d0cc7a1c46db543099
[wkunzhi] <362416272@qq.com>
2019-09-18 22:37:20 +0800

    新增Glidesky 2

--- 项目/Glidesky/login.py
+++ 项目/Glidesky/login.py
@@ -45,9 +45,34 @@ class Gli:
         print(int_list)
         print('爬虫基础1 合计', sum(int_list))
 
+    def no2(self):
+        """爬虫基础2
+        """
+        tasks = [asyncio.ensure_future(self.go_target(_)) for _ in range(1, 1001)]
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait(tasks))
+        print('爬虫基础2 合计', self.no2_count)
+
+    async def get_html(self, page):
+        async with aiohttp.ClientSession(headers=self.headers, cookies=self.cookies) as session:
+            result = await session.get(self.url_no2+str(page))
+            return await result.text()
+
+    async def go_target(self, page):
+        result = await self.get_html(page)
+        et = etree.HTML(result)
+        int_list = [int(i.strip()) for i in et.xpath('//*[@id="app"]/main/div/div/div/div/div/text()')]
+        self.no2_count += sum(int_list)
+        self.flag += 1
+        print('第{page}页, 已计算{pa}页,总和{count}'.format(page=page, count=self.no2_count, pa=self.flag), int_list)
+        if not int_list:
+            print(result)
+
 
 if __name__ == '__main__':
     username = ''
     password = ''
     g = Gli(username, password)
     g.no1()  # 基础1
+    # g.no2()
